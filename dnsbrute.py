7e8b83043504b0e51fb09bb5ab1c3667dd44ea26
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-07-02 19:24:32 -0400

    Adicionando a feature de receber o alvo como um argumento na inicialização do script

--- dnsbrute.py
+++ dnsbrute.py
@@ -1,16 +1,25 @@
 import dns.resolver
+import sys
 
-res = dns.resolver.Resolver()
-arquivo = open("/home/kali/myowntools/wordlist.txt", "r")
-subdominios = arquivo.read().splitlines()
 
-alvo = "globo.com.br"
+_resolver = dns.resolver.Resolver()
+_wordlist = open("/home/kali/myowntools/wordlist.txt", "r")
+_subdomains = _wordlist.read().splitlines()
 
-for subdominio in subdominios:
-    try:
-        sub_alvo = subdominio + "." + alvo
-        resultado = res.resolve(sub_alvo, "A")
-        for ip in resultado:
-            print(sub_alvo, "->", ip)
-    except:
-        pass
+def dns_brute(target):
+	print("Iniciando o processo de dnsbrute para o alvo: ", target)
+	for subdomain in _subdomains:
+		try:
+			sub_target = subdomain + "." + target
+			result = _resolver.resolve(sub_target, "A")
+			for ip in result:
+				print(sub_target, "->", ip)
+		except:
+			pass
+
+
+if __name__ == "__main__":
+	url = sys.argv[1]
+	dns_brute(url)
+
+
